03b355c5ffa6d6507cb5b1901c22b9f022a4e5d2
jeffarjeffar <jeffarjeffar@gmail.com>
2023-04-08 13:38:10 -0700

    format code

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,6 +4,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<Leader>fm"] = { ":lua vim.lsp.buf.format({ timeout_ms = 6969 })<CR>", "format code" },
   },
 }
 
