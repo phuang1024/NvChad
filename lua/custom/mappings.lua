43cb4dd58f756ca5cabf9309990e8a2da6b89078
jeffarjeffar <jeffarjeffar@gmail.com>
2023-04-09 08:32:42 -0700

    more mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,6 +5,12 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<Leader>fm"] = { ":lua vim.lsp.buf.format({ timeout_ms = 6969 })<CR>", "format code" },
+    ["<leader>rn"] = { vim.lsp.buf.rename, "[R]e[n]ame" },
+    ["<leader>ca"] = { vim.lsp.buf.code_action, "[C]ode [A]ction" },
+
+    ["gd"] = { vim.lsp.buf.definition, "[G]oto [D]efinition" },
+    ["gI"] = { vim.lsp.buf.implementation, "[G]oto [I]mplementation" },
+    ["<leader>D"] = { vim.lsp.buf.type_definition, "Type [D]efinition" },
   },
 }
 
