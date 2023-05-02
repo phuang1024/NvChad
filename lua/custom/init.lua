ae1b882ebe6fe87f6e17b5696641dd10f3f5de50
jeffarjeffar <jeffarjeffar@gmail.com>
2023-05-02 08:25:18 -0700

    fix clipboard

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -9,4 +9,5 @@ autocmd("VimResized", {
 vim.cmd('let g:vimtex_view_method = "zathura"')
 vim.cmd("let g:vimtex_format_enabled = 1")
 
-vim.cmd("let s:clip = '/mnt/c/Windows/System32/clip.exe'")
+-- WSL clipboard hack
+vim.cmd([[let g:clipboard = {"name": "WslClipboard","copy": {"+": "clip.exe","*": "clip.exe",},"paste": {"+": "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))","*": "powershell.exe -c [Console::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",},'cache_enabled': 0,}]])
